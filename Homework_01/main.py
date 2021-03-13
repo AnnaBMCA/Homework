abade4f39f7064301c7fbc1257c837bb1e8f0169
AnnaBMCA <anna@bmca.ru>
2021-03-13 20:41:22 +0300

    HW done

--- Homework_01/main.py
+++ Homework_01/main.py
@@ -2,14 +2,20 @@
 Домашнее задание №1
 Функции и структуры данных
 """
+my_list = [1, 6, 95, 69, 7, 24, 60]
+my_list_1 = [1, 3, 27, 69, 7, 24, 60]
 
 
-def power_numbers():
+def power_numbers(some_list):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     """
+    return [i ** 2 for i in some_list]
 
+
+power_numbers(my_list)
+print(power_numbers(my_list))
 
 # filter types
 ODD = "odd"
@@ -17,9 +23,24 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers():
+def filter_numbers(some_list, param):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
     (выбор производится передачей дополнительного аргумента)
     """
+    def is_prime(x):
+        for i in range(2, x):
+            if x % i == 0:
+                return False
+            return True
+
+    if param == "odd":
+        return list(filter(lambda x: x % 2 != 0, some_list))
+    if param == "even":
+        return list(filter(lambda x: x % 2 == 0, some_list))
+    if param == "prime":
+        return list(filter(is_prime, some_list))
+
+
+print(filter_numbers(my_list_1, PRIME))
